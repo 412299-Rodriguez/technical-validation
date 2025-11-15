77db28630fbb3dcb9c0dd71a0252a45349063fae
412299-Rodriguez <412299@tecnicatura.frc.utn.edu.ar>
2025-11-15 10:43:59 -0300

    chore: initial Spring Boot project setup

---
+++ backend/ficticia-client-service/ficticia-client-service/src/main/java/com/ficticia/ficticia_client_service/FicticiaClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ficticia.ficticia_client_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicticiaClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicticiaClientServiceApplication.class, args);
+	}
+
+}
---
+++ backend/ficticia-client-service/ficticia-client-service/src/test/java/com/ficticia/ficticia_client_service/FicticiaClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficticia.ficticia_client_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicticiaClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
